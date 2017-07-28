afc12e94a999427aa8753b10e737be87605aee62
Victor Diego <victordieggo@gmail.com>
2017-07-27 23:54:17 -0300

    Updated commentaries

--- includes/customizations/customize-wp-head.php
+++ includes/customizations/customize-wp-head.php
@@ -1,10 +1,13 @@
 <?php
 
-//======================================================================
+//===================================================================
 // FUNCTION -> CUSTOMIZE WP_HEAD
-//======================================================================
+//===================================================================
 
+//-------------------------------------------------------------------
 // REMOVE HEADER JUNK
+//-------------------------------------------------------------------
+
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'feed_links', 2);
@@ -21,12 +24,18 @@ remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'rel_canonical');
 remove_action('wp_head', 'wp_resource_hints', 2);
 
+//-------------------------------------------------------------------
 // REMOVE JSON/REST API
+//-------------------------------------------------------------------
+
 remove_action('wp_head', 'rest_output_link_wp_head');
 remove_action('wp_head', 'wp_oembed_add_discovery_links');
 remove_action('template_redirect', 'rest_output_link_header', 11, 0);
 
+//-------------------------------------------------------------------
 // REMOVE WP EMOJI
+//-------------------------------------------------------------------
+
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
