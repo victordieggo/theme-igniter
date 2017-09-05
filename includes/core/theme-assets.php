a17a6e4836d913e7d294f7b6dbb28fb08304d55c
Victor Diego <victordieggo@gmail.com>
2017-09-05 03:06:43 -0300

    Minor code standardization

--- includes/core/theme-assets.php
+++ includes/core/theme-assets.php
@@ -14,12 +14,12 @@ function theme_assets() {
 
   $directory = get_template_directory_uri();
 
-  wp_enqueue_style('styles', $directory."/assets/dist/css/style.css", false, '1.0.0', 'screen');
+  wp_enqueue_style('styles', $directory.'/assets/dist/css/style.css', false, '1.0.0', 'screen');
 
   wp_deregister_script('jquery');
   wp_deregister_script('wp-embed');
 
-  wp_enqueue_script('jquery', $directory."/assets/dist/js/jquery.min.js", false, '2.2.4', true);
-  wp_enqueue_script('scripts', $directory."/assets/dist/js/main.js", false, '1.0.0', true);
+  wp_enqueue_script('jquery', $directory.'/assets/dist/js/jquery.min.js', false, '2.2.4', true);
+  wp_enqueue_script('scripts', $directory.'/assets/dist/js/main.js', false, '1.0.0', true);
 
 }
