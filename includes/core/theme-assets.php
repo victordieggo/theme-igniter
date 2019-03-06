8247a04907aaffdc00d980906f9e750048fe1f3e
Victor Diego <victordieggo@gmail.com>
2019-03-06 16:45:21 -0300

    Add distinct functions to manage theme assets

--- includes/core/theme-assets.php
+++ includes/core/theme-assets.php
@@ -5,21 +5,29 @@
 //===================================================================
 
 //-------------------------------------------------------------------
-// ENQUEUE/DENQUEUE THEME STYLES/SCRIPTS
+// ENQUEUE THEME ASSETS
 //-------------------------------------------------------------------
 
-if (!is_admin()) add_action('wp_enqueue_scripts', 'theme_assets', 99999);
+add_action('wp_enqueue_scripts', 'theme_assets', 99);
 
 function theme_assets() {
+  if (is_admin()) return;
 
   $directory = get_template_directory_uri();
 
-  wp_enqueue_style('styles', $directory.'/assets/dist/css/style.css', false, '1.0.0', 'screen');
+  wp_enqueue_style('styles', $directory.'/assets/dist/css/main.css', false, '0.0.1', 'screen');
+  wp_enqueue_script('scripts', $directory.'/assets/dist/js/main.js', false, '0.0.1', true);
+}
+
+//-------------------------------------------------------------------
+// DENQUEUE/DEREGISTER UNUSED ASSETS
+//-------------------------------------------------------------------
+
+add_action('wp_enqueue_scripts', 'clean_assets', 99);
+
+function clean_assets() {
+  if (is_admin()) return;
 
   wp_deregister_script('jquery');
   wp_deregister_script('wp-embed');
-
-  wp_enqueue_script('jquery', $directory.'/assets/dist/js/jquery.min.js', false, '2.2.4', true);
-  wp_enqueue_script('scripts', $directory.'/assets/dist/js/main.js', false, '1.0.0', true);
-
 }
