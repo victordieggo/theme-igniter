0a4b52faed2902ba0f6c569c32f81de27274b236
Victor Diego <victordieggo@gmail.com>
2019-03-06 16:49:34 -0300

    Code formatting and standardisation

--- includes/customizations/customize-dashboard.php
+++ includes/customizations/customize-dashboard.php
@@ -30,6 +30,7 @@ function remove_menus() {
 //-------------------------------------------------------------------
 
 add_action('_admin_menu', 'remove_editor_menu', 1);
+
 function remove_editor_menu() {
   remove_action('admin_menu', '_add_themes_utility_last', 101);
 }
--- includes/customizations/customize-queries.php
+++ includes/customizations/customize-queries.php
@@ -9,6 +9,7 @@
 //-------------------------------------------------------------------
 
 add_action('pre_get_posts', 'limit_blog_posts');
+
 function limit_blog_posts($query) {
   if (is_home() || is_post_type_archive('post')) {
     $query->set('posts_per_page', 12);
