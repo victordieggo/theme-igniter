23a4ee3640f9fadd0638516cbedc089c7970a16d
victordieggo <victordieggo@gmail.com>
2020-07-23 20:26:47 -0300

    Update default theme assets

--- includes/core/theme-assets.php
+++ includes/core/theme-assets.php
@@ -13,21 +13,11 @@ add_action('wp_enqueue_scripts', 'theme_assets');
 function theme_assets() {
   if (is_admin()) return;
 
-  wp_enqueue_style('styles', ASSETS_URL . '/css/main.css', false, '0.0.1', 'screen');
-  wp_enqueue_script('scripts', ASSETS_URL . '/js/main.js', false, '0.0.1', true);
-}
-
-add_action('wp_enqueue_scripts', 'theme_assets', 99);
-
-//-------------------------------------------------------------------
-// DENQUEUE/DEREGISTER UNUSED ASSETS
-//-------------------------------------------------------------------
-
-add_action('wp_enqueue_scripts', 'clean_assets', 99);
-
-function clean_assets() {
-  if (is_admin()) return;
-
   wp_deregister_script('jquery');
   wp_deregister_script('wp-embed');
+
+  wp_dequeue_style('wp-block-library');
+
+  wp_enqueue_script('scripts', ASSETS_URL . '/js/main.js', false, '0.0.1', true);
+  wp_enqueue_style('styles', ASSETS_URL . '/css/main.css', false, '0.0.1', 'screen');
 }
